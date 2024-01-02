f25b5d3b9b48c62d9c5ecc2fb0f2b330857d4bc0
Dev <dev69440@gmail.com>
2024-01-03 01:41:57 +0530

    created build

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,11 +5,11 @@ import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  // const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+      {/* <div className="footer-div">
+        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           {emoji("Made with ")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
@@ -17,8 +17,8 @@ export default function Footer() {
           <a href="https://github.com/saadpasta/developerFolio">
             developerFolio
           </a>
-        </p> */}
-      </div>
+        </p>
+      </div> */}
     </Fade>
   );
 }
