fc0736dc528ac2fdc2299a8a0d46e6005d58d249
dev2842000 <dev69440@gmail.com>
2024-01-03 01:51:01 +0530

    checking push

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,6 +8,7 @@ export default function Footer() {
   // const {isDark} = useContext(StyleContext);
   return (
     <Fade bottom duration={1000} distance="5px">
+    
       {/* <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           {emoji("Made with ")}
